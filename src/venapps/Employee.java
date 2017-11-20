b4a9768e787056dc76059e20ae2c7ced760d5b20
Lenovo <Lenovo@lenovo.in.ibm.com>
2017-11-20 13:23:34 +0530

    1File is changed

---
+++ src/venapps/Employee.java
@@ -0,0 +1,34 @@
+package venapps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	public static void main(String[] args) {
+		System.out.println(">>>>>start the ArrafyList");
+		
+	List ll=new ArrayList();
+	ll.add("Lingaswamy");
+	ll.add("Hushi");
+
+	ll.add("Shiva");
+	ll.add("Ramu");
+	ll.add("Ramesh");
+	 System.out.println(ll);
+	 System.out.println(">>>>>End The Array List");
+
+	
+		 
+
+	}
+		 
+		 
+		 
+		 
+		
+	}
+	 
+	 
+	 
+
+
